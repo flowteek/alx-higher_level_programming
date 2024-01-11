ceefe0a01dfeb15bccf249a13be26809ede03769
samuel <samuelogunbanjo@gmail.com>
2024-01-11 00:56:24 -0800

    argument

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myArgument = process.argv.length -2;
+
+if (myArgument === 0) {
+	console.log('No argument');
+}else if (myArgument === 1) {
+	console.log('Argument found');
+}else {
+	console.log('Argument found');
+}
