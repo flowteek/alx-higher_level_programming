90807d4471081372d77d5f1ec5a20151b45ac6fb
samuel <samuelogunbanjo@gmail.com>
2024-01-13 02:11:02 -0800

    optional task

--- 0x13-javascript_objects_scopes_closures/100-data.js
+++ 0x13-javascript_objects_scopes_closures/100-data.js
@@ -1,2 +1,4 @@
 #!/usr/bin/node
-exports.list = [1, 2, 3, 4, 5];
+module.exports = {
+	list: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+};
--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,13 @@
 #!/usr/bin/node
-const list = require('./100-data.js').list;
-console.log(list);
-console.log(list.map((item, index) => item * index));
+// Importing the array from the file 100-data.js
+const { list } = require('./100-data');
+
+// Using the map function to create a new array based on the specified conditions
+const newList = list.map((value, index) => value * index);
+
+// Printing the initial list
+console.log("Initial List:", list);
+
+// Printing the new list
+console.log("New List:", newList);
+
